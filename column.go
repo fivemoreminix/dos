4f8ef64af5c8b86725c066f8af82e3d1fdd2c284
Luke Wilson <thelukaswils@gmail.com>
2021-10-06 12:53:27 -0500

    Completed basics of Column widget

--- column.go
+++ column.go
@@ -8,63 +8,75 @@ import (
 type Column struct {
 	Children        []Widget
 	HorizontalAlign Alignment
+	FocusedIndex    int // Index of child that receives focus
+	OnKeyEvent      func(ev *tcell.EventKey) bool
 }
 
-func (c Column) GetChildRects(currentRect Rect) []Rect {
-	if c.Children != nil {
-		rects := make([]Rect, 0, len(c.Children))
-		var rectsHeightSum int
-		for i := range c.Children {
-			childW, childH := c.Children[i].DisplaySize(currentRect.W, currentRect.H/len(c.Children))
-			// Allow child rects to overflow our currentRect, but later we resize them to fit
-			rects = append(rects, Rect{
-				currentRect.X + currentRect.W/2 - childW/2, // center child in X axis
-				rectsHeightSum,
-				childW,
-				childH,
-			})
-			rectsHeightSum += childH
+func (c *Column) GetChildRects(rect Rect) []Rect {
+	if childLen := len(c.Children); childLen > 0 {
+		rects := make([]Rect, childLen)
+		childHeightSum := 0
+		childMaxHeight := rect.H / childLen
+		for i := 0; i < childLen; i++ {
+			w, h := c.Children[i].DisplaySize(rect.W, childMaxHeight)
+			// TODO: implement HorizontalAlign for Column here
+			rects[i] = Rect{rect.X, 0, w, h}
+			childHeightSum += h
 		}
-		// Check if we overflowed the currentRect height
-		if rectsHeightSum > currentRect.H {
-			// Gives us the ratio to multiply by
-			scaleRatio := float64(currentRect.H) / float64(rectsHeightSum)
-			rectsHeightSum = 0
-			for i := 0; i < len(rects); i++ {
-				r := &rects[i]
-				r.Y = rectsHeightSum // Move rect up
-				r.H = int(float64(r.H) * scaleRatio)
-				rectsHeightSum += r.H
+		if childHeightSum < rect.H {
+			qualifyingChildren := 0
+			for i := 0; i < childLen; i++ {
+				if rects[i].H < childMaxHeight {
+					qualifyingChildren++
+				}
 			}
+			growAmount := (rect.H - childHeightSum) / qualifyingChildren
+			for i := 0; i < childLen; i++ {
+				if rects[i].H == childMaxHeight {
+					rects[i].H += growAmount
+				}
+			}
+		}
+		// Stack rects one on top of the other
+		childHeightSum = 0
+		for i := 0; i < childLen; i++ {
+			rects[i].Y = rect.Y + childHeightSum
+			childHeightSum += rects[i].H
 		}
 		return rects
 	}
 	return nil
 }
 
-func (c Column) HandleMouse(currentRect Rect, ev *tcell.EventMouse) bool {
-	if c.Children != nil {
-
-	}
-	return false
-}
-
-func (c Column) HandleKey(ev *tcell.EventKey) bool {
-	if c.Children != nil {
-
-	}
-	return false
-}
-
-func (c Column) SetFocused(b bool) {
-	if c.Children != nil {
-		for i := range c.Children {
-			c.Children[i].SetFocused(b)
+func (c *Column) HandleMouse(currentRect Rect, ev *tcell.EventMouse) bool {
+	for i := range c.Children {
+		if c.Children[i].HandleMouse(currentRect, ev) {
+			c.FocusedIndex = i
+			return true
 		}
 	}
+	return false
 }
 
-func (c Column) DisplaySize(boundsW, boundsH int) (w, h int) {
+func (c *Column) HandleKey(ev *tcell.EventKey) bool {
+	if c.OnKeyEvent != nil && c.OnKeyEvent(ev) {
+		return true
+	}
+	for i := range c.Children {
+		if c.Children[i].HandleKey(ev) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *Column) SetFocused(b bool) {
+	if c.FocusedIndex < len(c.Children) {
+		c.Children[c.FocusedIndex].SetFocused(b)
+	}
+}
+
+func (c *Column) DisplaySize(boundsW, boundsH int) (w, h int) {
 	rects := c.GetChildRects(Rect{0, 0, boundsW, boundsH})
 	if rects == nil {
 		return 0, 0
@@ -72,15 +84,17 @@ func (c Column) DisplaySize(boundsW, boundsH int) (w, h int) {
 	height := 0
 	width := 0
 	for i := range rects {
-		height += rects[i].H
+		height += rects[i].H // combined height
 		if rects[i].W > width {
-			width = rects[i].W
+			width = rects[i].W // only the maximum width
 		}
 	}
 	return width, height
-	// only as wide and tall as the combined width and height of all its children
 }
 
-func (c Column) Draw(rect Rect, s tcell.Screen) {
-	panic("implement me")
+func (c *Column) Draw(rect Rect, s tcell.Screen) {
+	rects := c.GetChildRects(rect)
+	for i := range rects {
+		c.Children[i].Draw(rects[i], s)
+	}
 }
